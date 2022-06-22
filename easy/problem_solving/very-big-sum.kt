175eb285722e9834219a29958414f70ecb104427
Hanif Setyananda <hanifsetya@dinustek.com>
2022-06-23 01:22:14 +0700

    from old sub

---
+++ easy/problem_solving/very-big-sum.kt
@@ -0,0 +1,46 @@
+
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'aVeryBigSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+ */
+
+fun aVeryBigSum(ar: Array<Long>): Long {
+    // Write your code here
+    var res = 0.toLong()
+    ar.forEach { angka ->
+        res += angka;
+     }
+     return res
+
+}
+
+fun main(args: Array<String>) {
+    val arCount = readLine()!!.trim().toInt()
+
+    val ar = readLine()!!.trimEnd().split(" ").map{ it.toLong() }.toTypedArray()
+
+    val result = aVeryBigSum(ar)
+
+    println(result)
+}
